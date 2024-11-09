f0c3b0bf48906e13f6bc132a66a2732b5759ae84
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-11-09 23:00:11 +0800

    Update hook.c

--- ZalithLauncher/src/main/jni/driver_helper/hook.c
+++ ZalithLauncher/src/main/jni/driver_helper/hook.c
@@ -19,9 +19,9 @@ static const char *sphal_namespaces[3] = {
 };
 
 // Constant
-static const char *vulkan_prefix = "vulkan.";
+__attribute__((visibility("default"), used)) static const char *vulkan_prefix = "vulkan.";
 
-__attribute__((visibility("default"), used)) void app__pojav_linkerhook_pass_handles(void* data, void* android_dlopen_ext,
+__attribute__((visibility("default"), used)) void app__pojav_linkerhook_pass_handles(void* data, static void* android_dlopen_ext,
                                                                                     void* android_get_exported_namespace) {
     ready_handle = data;
     android_dlopen_ext_p = android_dlopen_ext;
@@ -34,13 +34,13 @@ __attribute__((visibility("default"), used)) void *android_dlopen_ext(const char
     return ready_handle;
 }
 
-__attribute__((visibility("default"), used)) void *android_load_sphal_library(const char *filename, int flags) {
+__attribute__((visibility("default"), used)) void *android_load_sphal_library(const char *filename, static int flags) {
+    struct android_namespace_t* androidNamespace = NULL;
     if(strstr(filename, "vulkan.")) {
         return ready_handle;
     }
     //printf("__loader_android_get_exported_namespace = %p\n__loader_android_dlopen_ext = %p\n", __loader_android_get_exported_namespace,
     //       __loader_android_dlopen_ext);
-    struct android_namespace_t* androidNamespace;
     for(int i = 0; i < 3; i++) {
         androidNamespace = android_get_exported_namespace_p(sphal_namespaces[i]);
         if(androidNamespace != NULL) break;
@@ -54,6 +54,6 @@ __attribute__((visibility("default"), used)) void *android_load_sphal_library(co
 // This is done for older android versions which don't
 // export this function. Technically this is wrong
 // but for our usage it's fine enough
-__attribute__((visibility("default"), used)) uint64_t atrace_get_enabled_tags() {
+__attribute__((visibility("default"), used)) static uint64_t atrace_get_enabled_tags() {
     return 0;
 }
