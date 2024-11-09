688f56a2187994dda1fd4d86e8b29404aa0f6781
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-11-09 19:39:18 +0800

    Create bridge_tbl.c

---
+++ ZalithLauncher/src/main/jni/ctxbridges/bridge_tbl.c
@@ -0,0 +1,7 @@
+#include <ctxbridges/common.h>
+#include <ctxbridges/gl_bridge.h>
+#include <ctxbridges/osm_bridge.h>
+
+typedef basic_render_window_t* (*br_init_context_t)(basic_render_window_t* share);
+typedef void (*br_make_current_t)(basic_render_window_t* bundle);
+typedef basic_render_window_t* (*br_get_current_t)();
