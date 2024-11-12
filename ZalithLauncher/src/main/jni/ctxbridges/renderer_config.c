f13da73eba9f9ff1a0436b2bf1368f120ecfa1e8
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-11-12 12:43:32 +0800

    Update renderer_config.c

--- ZalithLauncher/src/main/jni/ctxbridges/renderer_config.c
+++ ZalithLauncher/src/main/jni/ctxbridges/renderer_config.c
@@ -1,10 +1,5 @@
 #include <EGL/egl.h>
 
-#define RENDERER_GL4ES 1
-#define RENDERER_VK_ZINK 2
-#define RENDERER_VIRGL 3
-#define RENDERER_VULKAN 4
-
 struct PotatoBridge {
     void* eglContext;    // EGLContext
     void* eglDisplay;    // EGLDisplay
