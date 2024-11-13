5f499ef7a5b69d3223677b79e77dc1672d33c62b
movte <212824502@qq.com>
2024-11-14 03:10:09 +0800

    Adjust[Language]: Microsoft account suspected of no Minecraft purchase

--- ZalithLauncher/src/main/java/net/kdt/pojavlaunch/authenticator/microsoft/PresentedException.java
+++ ZalithLauncher/src/main/java/net/kdt/pojavlaunch/authenticator/microsoft/PresentedException.java
@@ -2,22 +2,32 @@ package net.kdt.pojavlaunch.authenticator.microsoft;
 
 import android.content.Context;
 
+import com.movtery.zalithlauncher.R;
+import com.movtery.zalithlauncher.utils.stringutils.StringUtils;
+
 public class PresentedException extends RuntimeException {
     final int localizationStringId;
+    final boolean suspectedNoMinecraftPurchase;
     final Object[] extraArgs;
 
-    public PresentedException(int localizationStringId, Object... extraArgs) {
+    public PresentedException(int localizationStringId, boolean suspectedNoMinecraftPurchase, Object... extraArgs) {
         this.localizationStringId = localizationStringId;
+        this.suspectedNoMinecraftPurchase = suspectedNoMinecraftPurchase;
         this.extraArgs = extraArgs;
     }
 
-    public PresentedException(Throwable throwable, int localizationStringId, Object... extraArgs) {
+    public PresentedException(Throwable throwable, int localizationStringId, boolean suspectedNoMinecraftPurchase, Object... extraArgs) {
         super(throwable);
         this.localizationStringId = localizationStringId;
+        this.suspectedNoMinecraftPurchase = suspectedNoMinecraftPurchase;
         this.extraArgs = extraArgs;
     }
 
     public String toString(Context context) {
-        return context.getString(localizationStringId, extraArgs);
+        String string = context.getString(localizationStringId, extraArgs);
+        if (suspectedNoMinecraftPurchase) {
+            StringUtils.insertNewline(string, context.getString(R.string.no_minecraft_purchase));
+        }
+        return string;
     }
 }
