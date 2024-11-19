e7036e16b3c9cdcff55accc3555ac6ceb7a2801a
movte <212824502@qq.com>
2024-11-19 17:18:52 +0800

    Adjust[Animation]: Fine-tune the pop-up zoom animation effect

--- ZalithLauncher/src/main/java/com/movtery/anim/animations/bounce/BounceEnlargeAnimator.kt
+++ ZalithLauncher/src/main/java/com/movtery/anim/animations/bounce/BounceEnlargeAnimator.kt
@@ -9,7 +9,7 @@ class BounceEnlargeAnimator: BaseAnimator() {
     override fun getAnimators(target: View): Array<Animator> {
         return arrayOf(
             ObjectAnimator.ofFloat(target, "alpha", 0f, 0.8f, 1f, 1f),
-            ObjectAnimator.ofFloat(target, "scaleX", 0.7f, 1.03f, 0.95f, 1f),
-            ObjectAnimator.ofFloat(target, "scaleY", 0.7f, 1.03f, 0.95f, 1f))
+            ObjectAnimator.ofFloat(target, "scaleX", 0.7f, 1.03f, 0.97f, 1f),
+            ObjectAnimator.ofFloat(target, "scaleY", 0.7f, 1.03f, 0.97f, 1f))
     }
 }
