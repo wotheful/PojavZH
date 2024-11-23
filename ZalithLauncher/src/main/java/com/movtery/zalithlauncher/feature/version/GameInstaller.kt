8408917b42ff677fadeaf45039e263f385b299e4
movte <212824502@qq.com>
2024-11-23 17:52:33 +0800

    Fix[Install Game]: Error popup when installing vanilla without a custom version name

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/feature/version/GameInstaller.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/feature/version/GameInstaller.kt
@@ -48,7 +48,9 @@ class GameInstaller(
                         if (taskMap.isNotEmpty()) EventBus.getDefault().postSticky(installModVersion)
                         else {
                             //如果附加附件是空的，则表明只需要安装原版，需要确保这个自定义的版本文件夹内必定有原版的.json文件
-                            if (VersionsManager.isVersionExists(realVersion)) {
+                            //需要检查是否自定义了版本名，如果真实版本与自定义用户名相同，则表示用户并没有修改版本名，当前安装的就是纯原版
+                            //如果没有自定义用户名，则不复制版本文件，毕竟原版文件，与目标文件现在是同一个文件！
+                            if (realVersion != customVersionName && VersionsManager.isVersionExists(realVersion)) {
                                 //找到原版的.json文件，在MinecraftDownloader开始时，已经下载了
                                 val vanillaJsonFile = File(vanillaVersionFolder, "${vanillaVersionFolder.name}.json")
                                 if (vanillaJsonFile.exists() && vanillaJsonFile.isFile) {
@@ -56,6 +58,8 @@ class GameInstaller(
                                     FileUtils.copyFile(vanillaJsonFile, File(targetVersionFolder, "$customVersionName.json"))
                                 }
                             }
+                            //ModLoader任务为空，接下来的无意义ModLoader任务将彻底跳过！
+                            return@runTask null
                         }
 
                         //将Mod与Modloader的任务分离出来，应该先安装Mod
