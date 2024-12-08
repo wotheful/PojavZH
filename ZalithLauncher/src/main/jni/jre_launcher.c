ddfde744ffb48eea47a628a7a0ba61e75996c532
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-08 15:24:47 +0800

    Update jre_launcher.c

--- ZalithLauncher/src/main/jni/jre_launcher.c
+++ ZalithLauncher/src/main/jni/jre_launcher.c
@@ -37,6 +37,8 @@
 #include "utils.h"
 #include "environ/environ.h"
 
+
+if (getenv("JSP")) {
 // Uncomment to try redirect signal handling to JVM
 // #define TRY_SIG2JVM
 
@@ -234,4 +236,163 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
 
     LOGD("Free done");
     return res;
+}
+}
+
+if (!getenv("JSP")) {
+// Uncomment to try redirect signal handling to JVM
+// #define TRY_SIG2JVM
+
+// PojavLancher: fixme: are these wrong?
+#define FULL_VERSION "1.8.0-internal"
+#define DOT_VERSION "1.8"
+
+static const char* const_progname = "java";
+static const char* const_launcher = "openjdk";
+static const char** const_jargs = NULL;
+static const char** const_appclasspath = NULL;
+static const jboolean const_javaw = JNI_FALSE;
+static const jboolean const_cpwildcard = JNI_TRUE;
+static const jint const_ergo_class = 0; // DEFAULT_POLICY
+
+typedef jint JLI_Launch_func(int argc, char ** argv, /* main argc, argc */
+        int jargc, const char** jargv,          /* java args */
+        int appclassc, const char** appclassv,  /* app classpath */
+        const char* fullversion,                /* full version defined */
+        const char* dotversion,                 /* dot version defined */
+        const char* pname,                      /* program name */
+        const char* lname,                      /* launcher name */
+        jboolean javaargs,                      /* JAVA_ARGS */
+        jboolean cpwildcard,                    /* classpath wildcard*/
+        jboolean javaw,                         /* windows-only javaw */
+        jint ergo                               /* ergonomics class policy */
+);
+
+struct {
+    sigset_t tracked_sigset;
+    int pipe[2];
+} abort_waiter_data;
+
+_Noreturn extern void nominal_exit(int code, bool is_signal);
+
+_Noreturn static void* abort_waiter_thread(void* extraArg) {
+    pthread_sigmask(SIG_BLOCK, &abort_waiter_data.tracked_sigset, NULL);
+    int signal;
+    read(abort_waiter_data.pipe[0], &signal, sizeof(int));
+    nominal_exit(signal, true);
+}
+
+_Noreturn static void abort_waiter_handler(int signal) {
+    write(abort_waiter_data.pipe[1], &signal, sizeof(int));
+    while(1) {}
+}
+
+static void abort_waiter_setup() {
+    const static int tracked_signals[] = {SIGABRT};
+    const static int ntracked = (sizeof(tracked_signals) / sizeof(tracked_signals[0]));
+    struct sigaction sigactions[ntracked];
+    sigemptyset(&abort_waiter_data.tracked_sigset);
+
+    for (size_t i = 0; i < ntracked; i++)
+    {
+        sigaddset(&abort_waiter_data.tracked_sigset, tracked_signals[i]);
+        sigactions[i].sa_handler = abort_waiter_handler;
+    }
+
+    if (pipe(abort_waiter_data.pipe) != 0)
+    {
+        printf("Failed to set up aborter pipe: %s\n", strerror(errno));
+        return;
+    }
+
+    pthread_t waiter_thread; int result;
+
+    if ((result = pthread_create(&waiter_thread, NULL, abort_waiter_thread, NULL)) != 0)
+    {
+        printf("Failed to start up waiter thread: %s", strerror(result));
+        for(int i = 0; i < 2; i++) close(abort_waiter_data.pipe[i]);
+        return;
+    }
+
+    for (size_t i = 0; i < ntracked; i++)
+    {
+        if (sigaction(tracked_signals[i], &sigactions[i], NULL) != 0)
+        {
+            printf("Failed to set signal hander for signal %i: %s", i, strerror(errno));
+        }
+    }
+}
+
+static jint launchJVM(int margc, char** margv) {
+   void* libjli = dlopen("libjli.so", RTLD_LAZY | RTLD_GLOBAL);
+   struct sigaction clean_sa;
+   memset(&clean_sa, 0, sizeof (struct sigaction));
+
+   for (int sigid = SIGHUP; sigid < NSIG; sigid++)
+   {
+       if (sigid == SIGSEGV)
+           clean_sa.sa_handler = SIG_IGN;
+       else clean_sa.sa_handler = SIG_DFL;
+
+       sigaction(sigid, &clean_sa, NULL);
+   }
+
+   abort_waiter_setup();
+   // Boardwalk: silence
+   // LOGD("JLI lib = %x", (int)libjli);
+   if (NULL == libjli)
+   {
+       LOGE("JLI lib = NULL: %s", dlerror());
+       return -1;
+   }
+   LOGD("Found JLI lib");
+
+   JLI_Launch_func *pJLI_Launch = (JLI_Launch_func *)dlsym(libjli, "JLI_Launch");
+    // Boardwalk: silence
+    // LOGD("JLI_Launch = 0x%x", *(int*)&pJLI_Launch);
+
+   if (NULL == pJLI_Launch)
+   {
+       LOGE("JLI_Launch = NULL");
+       return -1;
+   }
+
+   LOGD("Calling JLI_Launch");
+
+   return pJLI_Launch(margc, margv,
+                   0, NULL, // sizeof(const_jargs) / sizeof(char *), const_jargs,
+                   0, NULL, // sizeof(const_appclasspath) / sizeof(char *), const_appclasspath,
+                   FULL_VERSION,
+                   DOT_VERSION,
+                   *margv, // (const_progname != NULL) ? const_progname : *margv,
+                   *margv, // (const_launcher != NULL) ? const_launcher : *margv,
+                   (const_jargs != NULL) ? JNI_TRUE : JNI_FALSE,
+                   const_cpwildcard, const_javaw, const_ergo_class);
+
+}
+
+JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, jclass clazz, jobjectArray argsArray) {
+    jint res = 0;
+
+    // Save dalvik JNIEnv pointer for JVM launch thread
+    pojav_environ->dalvikJNIEnvPtr_ANDROID = env;
+
+    if (argsArray == NULL)
+    {
+        LOGE("Args array null, returning");
+        return 0;
+    }
+
+    int argc = (*env)->GetArrayLength(env, argsArray);
+    char **argv = convert_to_char_array(env, argsArray);
+    LOGD("Done processing args");
+
+    res = launchJVM(argc, argv);
+
+    LOGD("Going to free args");
+    free_char_array(env, argsArray, argv);
+
+    LOGD("Free done");
+    return res;
+}
 }
