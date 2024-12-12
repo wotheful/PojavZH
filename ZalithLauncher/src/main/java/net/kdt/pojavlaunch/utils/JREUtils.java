29997984ab6b9b7113e96dc1e4e6fccf648e0324
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-12 23:52:21 +0800

    Update JREUtils.java

--- ZalithLauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ ZalithLauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -350,7 +350,7 @@ public class JREUtils {
         chdir(gameDirectory == null ? ProfilePathHome.getGameHome() : gameDirectory.getAbsolutePath());
         userArgs.add(0,"java"); //argv[0] is the program name according to C standard.
 
-        final int exitCode = VMLauncher.launchJVM(userArgs.toArray(new String[0]));
+        int exitCode = VMLauncher.launchJVM(userArgs.toArray(new String[0]));
         Logger.appendToLog("Java Exit code: " + exitCode);
         if (exitCode != 0) {
             File crashReportPath = new File(gameDirectory, "crash-reports");
