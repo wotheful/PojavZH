c5dba3550a2de27bdda5676868cf96def1475643
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-14 01:54:26 +0800

    Update loader_dlopen.h

--- ZalithLauncher/src/main/jni/ctxbridges/loader_dlopen.h
+++ ZalithLauncher/src/main/jni/ctxbridges/loader_dlopen.h
@@ -2,9 +2,9 @@
 // Created by maks on 26.10.2024.
 //
 
-#ifndef __POJAVLAUNCHER_LOADER_DLOPEN_H
-#define __POJAVLAUNCHER_LOADER_DLOPEN_H
+#ifndef __POJAVLAUNCHER_LOADER_DLOPEN_H_
+#define __POJAVLAUNCHER_LOADER_DLOPEN_H_
 
-void* loader_dlopen(char* primaryName, int flags);
+void* loader_dlopen(char* primaryName, char* secondaryName, int flags);
 
-#endif //__POJAVLAUNCHER_LOADER_DLOPEN_H
+#endif //__POJAVLAUNCHER_LOADER_DLOPEN_H_
