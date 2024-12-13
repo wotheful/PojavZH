cab2ca9cec3fa9681126c0a84ef916d37bfd0395
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-14 01:41:45 +0800

    Update loader_dlopen.c

--- ZalithLauncher/src/main/jni/ctxbridges/loader_dlopen.c
+++ ZalithLauncher/src/main/jni/ctxbridges/loader_dlopen.c
@@ -5,18 +5,11 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <linux/limits.h>
-void* loader_dlopen(char* primaryName, char* secondaryName, int flags) {
-    void* dl_handle;
-    if(primaryName == NULL) goto secondary;
+void* loader_dlopen(char* primaryName, int flags) {
+    void* dl_handle = NULL;
 
     dl_handle = dlopen(primaryName, flags);
     if(dl_handle != NULL) return dl_handle;
-    if(secondaryName == NULL) goto dl_error;
-
-    secondary:
-    dl_handle = dlopen(secondaryName, flags);
-    if(dl_handle == NULL) goto dl_error;
-    return dl_handle;
 
     dl_error:
     printf("%s", dlerror());
