dac0ae926f56cd28bd7a1aea51d6c2d3a02b3b22
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-15 01:32:10 +0800

    Update VMLauncher.java

--- ZalithLauncher/src/main/java/com/oracle/dalvik/VMLauncher.java
+++ ZalithLauncher/src/main/java/com/oracle/dalvik/VMLauncher.java
@@ -1,12 +1,9 @@
 package com.oracle.dalvik;
 
-import androidx.annotation.Keep;
-import dalvik.annotation.optimization.CriticalNative;
-
 public final class VMLauncher {
 	private VMLauncher() {
 	}
-	@Keep @CriticalNative public static native int launchJVM(String[] args);
+	public static native int launchJVM(String[] args);
 
 	static {
 		System.loadLibrary("pojavexec");
