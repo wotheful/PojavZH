87cb22f456534199750a471ce23b11ca7439e9d9
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-16 22:59:25 +0800

    Update gl_bridge.c

--- ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
+++ ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
@@ -42,7 +42,37 @@ gl_render_window_t* gl_get_current(void) {
     return currentBundle;
 }
 
+static void gl4esi_get_display_dimensions(int* width, int* height) {
+    if (currentBundle == NULL) goto zero;
+    EGLSurface surface = currentBundle->surface;
+    EGLBoolean result_width = eglQuerySurface_p(g_EglDisplay, surface, EGL_WIDTH, width);
+    EGLBoolean result_height = eglQuerySurface_p(g_EglDisplay, surface, EGL_HEIGHT, height);
+    if (!result_width || !result_height) goto zero;
+    return;
+
+    zero:
+    *width = 0;
+    *height = 0;
+}
+
 static bool already_initialized = false;
+void gl_init_gl4es_internals(void) {
+    if(already_initialized) return;
+    already_initialized = true;
+    void* gl4es = dlopen("libgl4es_114.so", RTLD_NOLOAD);
+    if(gl4es == NULL) return;
+    void (*set_getmainfbsize)(void (*new_getMainFBSize)(int* width, int* height));
+    set_getmainfbsize = dlsym(gl4es, "set_getmainfbsize");
+    if(set_getmainfbsize == NULL) goto warn;
+    set_getmainfbsize(gl4esi_get_display_dimensions);
+    goto cleanup;
+
+    warn:
+    printf("gl4esinternals warning: gl4es was found but internals not initialized. expect rendering issues.\n");
+    cleanup:
+    // dlclose just decreases a ref counter, so this is fine
+    dlclose(gl4es);
+}
 
 gl_render_window_t* gl_init_context(gl_render_window_t *share) {
     gl_render_window_t* bundle = malloc(sizeof(gl_render_window_t));
