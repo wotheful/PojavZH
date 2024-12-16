79c81262faa5f695ec06e3551e1e8d4ccbb46ef1
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-12-16 22:56:24 +0800

    Update egl_bridge.c

--- ZalithLauncher/src/main/jni/egl_bridge.c
+++ ZalithLauncher/src/main/jni/egl_bridge.c
@@ -29,6 +29,7 @@
 #include <android/dlext.h>
 #include "utils.h"
 #include "ctxbridges/bridge_tbl.h"
+#include "ctxbridges/gl_bridge.h"
 #include "ctxbridges/osm_bridge.h"
 
 #define GLFW_CLIENT_API 0x22001
@@ -46,38 +47,6 @@ struct PotatoBridge potatoBridge;
 
 
 static void* gbuffer;
-
-static void gl4esi_get_display_dimensions(int* width, int* height) {
-    if (currentBundle == NULL) goto zero;
-    EGLSurface surface = currentBundle->surface;
-    EGLBoolean result_width = eglQuerySurface_p(g_EglDisplay, surface, EGL_WIDTH, width);
-    EGLBoolean result_height = eglQuerySurface_p(g_EglDisplay, surface, EGL_HEIGHT, height);
-    if (!result_width || !result_height) goto zero;
-    return;
-
-    zero:
-    *width = 0;
-    *height = 0;
-}
-
-static bool already_initialized = false;
-static void gl_init_gl4es_internals(void) {
-    if(already_initialized) return;
-    already_initialized = true;
-    void* gl4es = dlopen("libgl4es_114.so", RTLD_NOLOAD);
-    if(gl4es == NULL) return;
-    void (*set_getmainfbsize)(void (*new_getMainFBSize)(int* width, int* height));
-    set_getmainfbsize = dlsym(gl4es, "set_getmainfbsize");
-    if(set_getmainfbsize == NULL) goto warn;
-    set_getmainfbsize(gl4esi_get_display_dimensions);
-    goto cleanup;
-
-    warn:
-    printf("gl4esinternals warning: gl4es was found but internals not initialized. expect rendering issues.\n");
-    cleanup:
-    // dlclose just decreases a ref counter, so this is fine
-    dlclose(gl4es);
-}
 
 EXTERNAL_API void pojavTerminate(void) {
     printf("EGLBridge: Terminating\n");
