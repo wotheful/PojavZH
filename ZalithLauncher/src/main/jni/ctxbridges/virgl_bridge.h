62afdbfd6f23aaf9106c905812dd39935cc4bfb6
Vera-Firefly <1964762970@qq.com>
2024-12-26 11:02:07 +0800

    Feat[Virgl]: Mesa 22.0.5 -> 21.2.1 & Update virgl bridge

--- ZalithLauncher/src/main/jni/ctxbridges/virgl_bridge.h
+++ ZalithLauncher/src/main/jni/ctxbridges/virgl_bridge.h
@@ -5,12 +5,22 @@
 #ifndef VIRGL_BRIDGE_H
 #define VIRGL_BRIDGE_H
 
-void* virglGetCurrentContext();
-void loadSymbolsVirGL();
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include "osmesa_loader.h"
+
+struct virgl_render_window_t {
+    struct ANativeWindow *nativeSurface;
+    ANativeWindow_Buffer buffer;
+    OSMesaContext context;
+};
+
+bool loadSymbolsVirGL();
 int virglInit();
-void virglSwapBuffers();
-void virglMakeCurrent(void* window);
 void* virglCreateContext(void* contextSrc);
+void* virglGetCurrentContext();
+void virglMakeCurrent(void* window);
+void virglSwapBuffers();
 void virglSwapInterval(int interval);
 
 #endif //VIRGL_BRIDGE_H
