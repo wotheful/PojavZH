6fb727ff815e5fe6d03305a04a2aa52439160197
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-17 11:29:08 +0800

    Update egl_bridge.c

--- ZalithLauncher/src/main/jni/egl_bridge.c
+++ ZalithLauncher/src/main/jni/egl_bridge.c
@@ -237,6 +237,7 @@ static int pojavInitOpenGL(void) {
         if (!strcmp(getenv("OSMESA_NO_FLUSH_FRONTBUFFER"), "0"))
             printf("VirGL: OSMesa buffer flush is ENABLED!\n");
         loadSymbolsVirGL();
+        virglInit();
         return 0;
     }
 
