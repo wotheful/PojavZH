3cd004642c97f3c68886aeb1164f29262b3e30ff
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-18 14:40:15 +0800

    Update Android.mk

--- ZalithLauncher/src/main/jni/Android.mk
+++ ZalithLauncher/src/main/jni/Android.mk
@@ -27,6 +27,16 @@ LOCAL_LDLAGS += -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -fuse-ld=lld
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := tinywrapper
+LOCAL_SRC_FILES := angle/main.c angle/string_utils.c
+LOCAL_SHARED_LIBRARIES := GLESv3
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/angle
+# LOCAL_CFLAGS += -rdynamic
+LOCAL_CFLAGS += -O3 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -Weverything -std=c2x -fno-emulated-tls -march=armv8-a+simd+crc+crypto+fp16 -mcpu=cortex-a76 -fdata-sections -ffunction-sections -fmerge-all-constants
+LOCAL_LDLAGS += -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -fuse-ld=lld
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 # Link GLESv2 for test
 LOCAL_LDLIBS := -ldl -llog -landroid
 # -lGLESv2
