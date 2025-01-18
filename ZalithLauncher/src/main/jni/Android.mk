5cec84a914e1a1cb5e9bf539b900b10ca569b142
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-18 08:32:07 +0800

    Update Android.mk

--- ZalithLauncher/src/main/jni/Android.mk
+++ ZalithLauncher/src/main/jni/Android.mk
@@ -21,7 +21,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Angle
 LOCAL_SRC_FILES := angle/main.c angle/string_utils.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/angle
-LOCAL_CFLAGS += -rdynamic
+# LOCAL_CFLAGS += -rdynamic
 LOCAL_CFLAGS += -O3 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -Weverything -std=c2x -fno-emulated-tls -march=armv8-a+simd+crc+crypto+fp16 -mcpu=cortex-a76 -fdata-sections -ffunction-sections -fmerge-all-constants
 LOCAL_LDLAGS += -flto=thin -Wl,-plugin-opt=-emulated-tls=0 -fuse-ld=lld
 LOCAL_LDLIBS += -lGLESv3
