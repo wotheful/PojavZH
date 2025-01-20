13eeaaf025fcb7f27d80946ab7097effb3c68f9f
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-21 00:51:17 +0800

    Update gl_bridge.c

--- ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
+++ ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
@@ -47,7 +47,7 @@ gl_render_window_t* gl_init_context(gl_render_window_t *share) {
     gl_render_window_t* bundle = malloc(sizeof(gl_render_window_t));
     memset(bundle, 0, sizeof(gl_render_window_t));
     EGLint egl_attributes[] = { EGL_NONE };
-    if (!strcmp(getenv("POJAV_RENDERER"), "opengles3_gl4es_plus", 20)
+    if (!strcmp(getenv("POJAV_RENDERER"), "opengles3_gl4es_plus")
      || !strncmp(getenv("POJAV_RENDERER"), "opengles3_desktopgl", 19))
     {
       EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_ALPHA_MASK_SIZE, 8, EGL_SURFACE_TYPE, EGL_WINDOW_BIT|EGL_PBUFFER_BIT, EGL_CONFORMANT, EGL_OPENGL_ES2_BIT, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE };
