eafbf708289f6ca82dd68444b42f65a18eced2d5
wotheful <hi114514@petalmail.com>
2025-01-27 21:27:27 +0800

    Update jre_launcher.c

--- ZalithLauncher/src/main/jni/jre_launcher.c
+++ ZalithLauncher/src/main/jni/jre_launcher.c
@@ -44,22 +44,20 @@
 #define FULL_VERSION "1.8.0-internal"
 #define DOT_VERSION "1.8"
 
-__attribute__((unused)) static const char* const_progname = "java";
-__attribute__((unused)) static const char* const_launcher = "openjdk";
+static const char* const_progname = "java";
+static const char* const_launcher = "openjdk";
 static const char** const_jargs = NULL;
-__attribute__((unused)) static const char** const_appclasspath = NULL;
+static const char** const_appclasspath = NULL;
 static const jboolean const_javaw = JNI_FALSE;
 static const jboolean const_cpwildcard = JNI_TRUE;
 static const jint const_ergo_class = 0; // DEFAULT_POLICY
 static struct sigaction old_sa[NSIG];
 
-static void (*__old_sa)(int signal, siginfo_t *info, void *reserved);
-static int (*sigaction_p) (int signum,
-              const struct sigaction *_Nullable restrict act,
-              struct sigaction *_Nullable restrict oldact);
-static int (*JVM_handle_linux_signal)(int signo, siginfo_t* siginfo, void* ucontext, int abort_if_unrecognized);
+void (*__old_sa)(int signal, siginfo_t *info, void *reserved);
+int (*JVM_handle_linux_signal)(int signo, siginfo_t* siginfo, void* ucontext, int abort_if_unrecognized);
 
-static void android_sigaction(int signal, siginfo_t *info, void *reserved) {
+void android_sigaction(int signal, siginfo_t *info, void *reserved) {
+  printf("process killed with signal %d code %p addr %p\n", signal,info->si_code,info->si_addr);
   if (JVM_handle_linux_signal == NULL) { // should not happen, but still
       __old_sa = old_sa[signal].sa_sigaction;
       __old_sa(signal,info,reserved);
@@ -88,21 +86,21 @@ typedef jint JLI_Launch_func(int argc, char ** argv, /* main argc, argc */
 
 static jint launchJVM(int margc, char** margv) {
    void* libjli = dlopen("libjli.so", RTLD_LAZY | RTLD_GLOBAL);
+
    // Boardwalk: silence
    // LOGD("JLI lib = %x", (int)libjli);
-   if (NULL == libjli)
-   {
+   if (NULL == libjli) {
        LOGE("JLI lib = NULL: %s", dlerror());
        return -1;
    }
    LOGD("Found JLI lib");
 
-   JLI_Launch_func *pJLI_Launch = (JLI_Launch_func *)dlsym(libjli, "JLI_Launch");
+   JLI_Launch_func *pJLI_Launch =
+          (JLI_Launch_func *)dlsym(libjli, "JLI_Launch");
     // Boardwalk: silence
     // LOGD("JLI_Launch = 0x%x", *(int*)&pJLI_Launch);
 
-   if (NULL == pJLI_Launch)
-   {
+   if (NULL == pJLI_Launch) {
        LOGE("JLI_Launch = NULL");
        return -1;
    }
@@ -118,18 +116,21 @@ static jint launchJVM(int margc, char** margv) {
                    *margv, // (const_launcher != NULL) ? const_launcher : *margv,
                    (const_jargs != NULL) ? JNI_TRUE : JNI_FALSE,
                    const_cpwildcard, const_javaw, const_ergo_class);
-
+/*
+   return pJLI_Launch(argc, argv, 
+       0, NULL, 0, NULL, FULL_VERSION,
+       DOT_VERSION, *margv, *margv, // "java", "openjdk",
+       JNI_FALSE, JNI_TRUE, JNI_FALSE, 0);
+*/
 }
 
+/*
+ * Class:     com_oracle_dalvik_VMLauncher
+ * Method:    launchJVM
+ * Signature: ([Ljava/lang/String;)I
+ */
 JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, jclass clazz, jobjectArray argsArray) {
 #ifdef TRY_SIG2JVM
-  void* libjsig = dlopen("libjsig.so", RTLD_LAZY | RTLD_GLOBAL);
-  if (NULL == libjsig) {
-      LOGE("JSig lib = NULL: %s", dlerror());
-      return -1;
-  }
-  sigaction_p = (void*) dlsym(libjsig, "sigaction");
-
   void* libjvm = dlopen("libjvm.so", RTLD_LAZY | RTLD_GLOBAL);
   if (NULL == libjvm) {
       LOGE("JVM lib = NULL: %s", dlerror());
@@ -137,6 +138,7 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
   }
   JVM_handle_linux_signal = dlsym(libjvm, "JVM_handle_linux_signal");
 #endif
+
    jint res = 0;
    // int i;
    //Prepare the signal trapper
@@ -145,9 +147,9 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
    catcher.sa_sigaction = android_sigaction;
    catcher.sa_flags = SA_SIGINFO|SA_RESTART;
    // SA_RESETHAND;
-#define CATCHSIG(X) sigaction_p(X, &catcher, &old_sa[X])
+#define CATCHSIG(X) sigaction(X, &catcher, &old_sa[X])
     CATCHSIG(SIGILL);
-    CATCHSIG(SIGABRT);
+    //CATCHSIG(SIGABRT);
     CATCHSIG(SIGBUS);
     CATCHSIG(SIGFPE);
 #ifdef TRY_SIG2JVM
@@ -161,14 +163,15 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
     // Save dalvik JNIEnv pointer for JVM launch thread
     pojav_environ->dalvikJNIEnvPtr_ANDROID = env;
 
-    if (argsArray == NULL)
-    {
+    if (argsArray == NULL) {
         LOGE("Args array null, returning");
+        //handle error
         return 0;
     }
 
     int argc = (*env)->GetArrayLength(env, argsArray);
     char **argv = convert_to_char_array(env, argsArray);
+
     LOGD("Done processing args");
 
     res = launchJVM(argc, argv);
@@ -177,5 +180,6 @@ JNIEXPORT jint JNICALL Java_com_oracle_dalvik_VMLauncher_launchJVM(JNIEnv *env, 
     free_char_array(env, argsArray, argv);
 
     LOGD("Free done");
+
     return res;
 }
